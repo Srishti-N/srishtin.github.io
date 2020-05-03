7f1b7182b8fa59751d6189700a8e3d2a7dc79e20
Srishti-N <58177116+Srishti-N@users.noreply.github.com>
2020-05-03 16:39:55 +0100

    Tried to add pause/play button
    Had a re-read of the carousel features and missed some info about the play/pause buttons. Still doesn't work, console log showing errors about (setSlide interval) definitions.

--- carousel.js
+++ carousel.js
@@ -1,6 +1,7 @@
 // Hamburger naviagation //
 
-const hamburger = document.querySelector(".hamburger"); //Made a mistake here. Missed '.' and spent hours trying to fix it
+
+const hamburger = document.querySelector(".hamburger"); 
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
@@ -8,56 +9,107 @@ hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("open");
 });
 
+//Made a mistake here. Missed ""."hamburger and spent hours trying to fix it //
+// I made a mistake initally when I started to build the hamburger navigation,
+// but I thought it looked quite cool//
+//So decided to make it a part of my the design and into a landing page//
+
+
+
 // END OF NAV//
 
-//Begin Carousel 
- const carouselSlide = document.querySelector(".carousel-slide");
- const carouselImages = document.querySelectorAll(".carousel-slide img");
+//Begin Carousel //
 
 
- //Buttons
 
- const prevButton = document.querySelector("#prevbutton");
- const nextButton = document.querySelector("#nextbutton");
+const carouselSlide = document.querySelector(".carousel-slide");
+const carouselImages = document.querySelectorAll(".carousel-slide img");
 
- let counter = 1;
- const size = carouselImages[0].clientWidth;
 
- carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+//Buttons //
+
+const prevButton = document.querySelector("#prevbutton");
+const nextButton = document.querySelector("#nextbutton");
+
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
 
 
 nextButton.addEventListener("click", () => {
-    if (counter >= carouselImages.lenth -1) return;
-    carouselSlide.style.transition = "transform 0.5s ease-in-out";
-    counter++;   
-    carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+   if (counter >= carouselImages.lenth -1) return;
+   carouselSlide.style.transition = "transform 0.5s ease-in-out";
+   counter++;   
+   carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
 
 });
 
 prevButton.addEventListener("click", () => {
-    if (counter <= 0) return;
-    carouselSlide.style.transition = "transform 0.5s ease-in-out";
-    counter--;   
-    carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+   if (counter <= 0) return;
+   carouselSlide.style.transition = "transform 0.5s ease-in-out";
+   counter--;   
+   carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
 
 });
 const lastClone = document.getElementById("lastClone");
 const firstClone =document.getElementById("firstClone");
 
 carouselSlide.addEventListener("transitionend", () => {
-    if (carouselImages[counter] === lastClone){
-        carouselSlide.style.transition = "none";
-        counter = carouselImages.length - 2;
-        carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+   if (carouselImages[counter] === lastClone){
+       carouselSlide.style.transition = "none";
+       counter = carouselImages.length - 2;
+       carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
 
-    }
-    if (carouselImages[counter] === firstClone) {
-        carouselSlide.style.transition = "none";
-        counter = carouselImages.length - counter;
-        carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
-    }
+   }
+   if (carouselImages[counter] === firstClone) {
+       carouselSlide.style.transition = "none";
+       counter = carouselImages.length - counter;
+       carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+   }
 });
 
-//everything leaves the page if you click too fast...so please click slowly for optimal scrolling experience
-//Increased transition to 1s, bit glitchy but you can't leave the page now
-// 1s was too slow for width of image
+var playing = true;
+var pauseButton = document.getElementById("play-pause");
+var myIndex = 0;
+var slideInterval = setInterval(nextSlide,2000);
+
+function carousel () {
+    var i;
+    var x = document.getElementsByClassName(".carousel-slide");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.lenth) {
+        myIndex = 1
+    }
+x[myIndex - 1].style.display = "block";
+}
+
+function playSlideshow(){
+    pauseButton.innerHTML = "Play";
+    playing = false;
+    clearInterval(slideInterval);
+}
+
+function pauseSlideshow () {
+    pauseButton.innerHTML = "Pause";
+    playing = true;
+    slideInterval = setInterval(nextSlide,2000);
+}
+
+pauseButton.onclick = function () {
+    if(playing) {
+        pauseSlideshow();
+    }
+    else {
+        playSlideshow();
+    }
+};
+
+carousel();
+playSlideshow();
+
+
+
